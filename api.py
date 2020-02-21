3f859b7d2a5d396615b7adf3ca4f038937b2fa41
Leo <lmcguire@crefeld.org>
2020-02-21 11:21:05 -0500

    Added /student/<student_name>/tracker/ route to app student_tracker() renders and returns jinja_templates/tracker_template.html, which will be added in later commits

--- api.py
+++ api.py
@@ -7,5 +7,14 @@ app = fl.Flask(__name__, template_folder='jinja_templates/')
 with open('user-data/student_trackers.json') as f:
 	student_profiles = json.load(f)
 
+@app.route('/student/<student_name>/tracker/')
+def student_tracker(student_name):
+	try:
+		student = student_profiles[student_name]
+		template_context = dict(student=student, student_name=student_name)
+		return fl.render_template('tracker_template.html', **template_context)
+	except KeyError:
+		abort(404, f'Student {student_name} not found')
+
 if __name__ == "__main__":
 	app.run(debug=True)
